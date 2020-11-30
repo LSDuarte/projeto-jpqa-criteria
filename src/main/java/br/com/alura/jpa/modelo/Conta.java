4c6c130f0907a5302db9b2ee79874071493f434b
Larissa Duarte <42499@pjmt.local>
2020-11-30 19:44:32 -0400

    adicionando arquivo teste

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,9 +1,12 @@
 package br.com.alura.jpa.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Conta {
@@ -15,6 +18,9 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private Double saldo;
+	
+	@OneToMany
+	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
@@ -50,5 +56,9 @@ public class Conta {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
+	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return movimentacoes;
 	}
 }
